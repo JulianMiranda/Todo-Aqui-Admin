6e31ed1ec439a923f1b700624e8da286cab70012
JulianMiranda <jmirandauria@gmail.com>
2020-06-29 09:31:35 -0500

    CategoriasShow

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -2,10 +2,12 @@ import BusinessIcon from '@material-ui/icons/Business';
 import CategoriesList from './CategoriesList';
 import CategoriesCreate from './CategoriesCreate';
 import CategoriesEdit from './CategoriesEdit';
+import CategoriesShow from './CategoriesShow';
 
 export default {
 	list: CategoriesList,
 	icon: BusinessIcon,
 	edit: CategoriesEdit,
 	create: CategoriesCreate,
+	show: CategoriesShow,
 };
