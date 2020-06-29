699990edca9409780ef63cfba002b4d6178a0200
JulianMiranda <jmirandauria@gmail.com>
2020-06-29 09:36:28 -0500

    Subcategorias Show

--- src/components/subcategories/index.js
+++ src/components/subcategories/index.js
@@ -2,10 +2,12 @@ import HomeWorkIcon from '@material-ui/icons/HomeWork';
 import SubCategoriesList from './SubCategoriesList';
 import SubCategoriesEdit from './SubCategoriesEdit';
 import SubCategoriesCreate from './SubCategoriesCreate';
+import SubCategoriesShow from './SubCategoriesShow';
 
 export default {
 	icon: HomeWorkIcon,
 	list: SubCategoriesList,
 	edit: SubCategoriesEdit,
 	create: SubCategoriesCreate,
+	show: SubCategoriesShow,
 };
