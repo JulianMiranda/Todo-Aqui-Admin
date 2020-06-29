cc4874a12806b99eed8b1978b64e7f08729cc602
JulianMiranda <jmirandauria@gmail.com>
2020-06-29 10:59:33 -0500

    Edit de Categorias y SubCat Arreglado

--- src/components/categories/CategoriesEdit.js
+++ src/components/categories/CategoriesEdit.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import {Edit, TextInput, SimpleForm, BooleanInput} from 'react-admin';
+import {Edit, TextInput, Toolbar, FormWithRedirect, BooleanInput} from 'react-admin';
 import {Grid, CardContent, Card} from '@material-ui/core';
 import {CategoriesTitle} from './CategoriesTitle';
 
 const CategoriesEdit = (props) => {
 	return (
-		<Edit {...props} title={<CategoriesTitle />}>
-			<SimpleForm redirect="list">
-				<Card>
-					<CardContent>
-						<Grid container spacing={2}>
-							<Grid item sm={6} xs={12}>
-								<TextInput label="Nombre" fullWidth source="name" />
-							</Grid>{' '}
-							<Grid item sm={12} xs={12}>
-								<BooleanInput label="Estado" source="status" />
-							</Grid>
-						</Grid>
-					</CardContent>
-				</Card>
-			</SimpleForm>
+		<Edit {...props} title={<CategoriesTitle />} component="div">
+			<CategoriesForm />
 		</Edit>
 	);
 };
+const CategoriesForm = (props) => {
+	return (
+		<FormWithRedirect
+			{...props}
+			render={(formProps) => (
+				<Card>
+					<form>
+						<CardContent>
+							<Grid container spacing={2}>
+								<Grid item sm={6} xs={12}>
+									<TextInput label="Nombre" source="name" />
+								</Grid>{' '}
+								<Grid item sm={12} xs={12}>
+									<BooleanInput label="Estado" source="status" />
+								</Grid>
+							</Grid>
+						</CardContent>
+						<Toolbar
+							record={formProps.record}
+							basePath={formProps.basePath}
+							undoable={true}
+							invalid={formProps.invalid}
+							handleSubmit={formProps.handleSubmit}
+							saving={formProps.saving}
+							resource="categories"
+						/>
+					</form>
+				</Card>
+			)}
+		/>
+	);
+};
+
 export default CategoriesEdit;
--- src/components/subcategories/SubCategoriesEdit.js
+++ src/components/subcategories/SubCategoriesEdit.js
@@ -6,7 +6,8 @@ import {
 	ReferenceInput,
 	ImageField,
 	SelectInput,
-	SimpleForm,
+	Toolbar,
+	FormWithRedirect,
 	BooleanInput,
 } from 'react-admin';
 import {SubCategoriesTitle} from './SubCategoriesTitle';
@@ -14,42 +15,58 @@ import {Grid, CardContent, Card} from '@material-ui/core';
 
 const SubCategoriesEdit = (props) => {
 	return (
-		<Edit {...props} title={<SubCategoriesTitle />}>
-			<SimpleForm redirect="list">
-				<Card>
-					<CardContent>
-						<Grid container spacing={2}>
-							<Grid item sm={6} xs={12}>
-								<TextInput label="Nombre" fullWidth source="name" />
-							</Grid>{' '}
-							<Grid item sm={6} xs={12}>
-								<ReferenceInput
-									label="Categoría"
-									fullWidth
-									source="category.id"
-									reference="categories"
-								>
-									<SelectInput source="name" />
-								</ReferenceInput>
-							</Grid>
-							<Grid item sm={12} xs={12}>
-								<ImageInput
-									source="image"
-									resource="subcategories"
-									accept="image/*"
-									placeholder={<p>Drop your file here</p>}
-									fullWidth
-								>
-									<ImageField source="url" />
-								</ImageInput>
-
-								<BooleanInput label="Estado" source="status" />
-							</Grid>
-						</Grid>
-					</CardContent>
-				</Card>
-			</SimpleForm>
+		<Edit {...props} title={<SubCategoriesTitle />} component="div">
+			<SubCategoriesForm />
 		</Edit>
+	);
+};
+
+const SubCategoriesForm = (props) => {
+	return (
+		<FormWithRedirect
+			{...props}
+			render={(formProps) => (
+				<Card>
+					<form>
+						<CardContent>
+							<Grid container spacing={2}>
+								<Grid item sm={6} xs={12}>
+									<TextInput label="Nombre" source="name" />
+								</Grid>{' '}
+								<Grid item sm={6} xs={12}>
+									<ReferenceInput label="Categoría" source="category.id" reference="categories">
+										<SelectInput source="name" />
+									</ReferenceInput>
+								</Grid>
+								<Grid item sm={6} xs={12}>
+									<ImageInput
+										source="image"
+										resource="subcategories"
+										accept="image/*"
+										placeholder={<p>Drop your file here</p>}
+									>
+										<ImageField source="url" />
+									</ImageInput>
+								</Grid>
+								<Grid item sm={6} xs={12}>
+									<BooleanInput label="Estado" source="status" />
+								</Grid>
+							</Grid>
+						</CardContent>
+
+						<Toolbar
+							record={formProps.record}
+							basePath={formProps.basePath}
+							undoable={true}
+							invalid={formProps.invalid}
+							handleSubmit={formProps.handleSubmit}
+							saving={formProps.saving}
+							resource="subcategories"
+						/>
+					</form>
+				</Card>
+			)}
+		/>
 	);
 };
 export default SubCategoriesEdit;
